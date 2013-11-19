793108fc99941c9180a1b11088886becc360967d
Will Fisher <will@teknofire.net>
2013-11-19 14:20:02 -0900

    adding page params to api

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -1,6 +1,6 @@
 class SitesController < ApplicationController
   def index
-    @sites = Site.limit(api_params[:limit])
+    @sites = Site.limit(api_params[:limit]).offset(api_params[:start])
     
     respond_to do |format|
       format.html
@@ -26,9 +26,12 @@ class SitesController < ApplicationController
   protected
   
   def api_params
-    api_request = params.permit(:limit)
+    api_request = params.permit(:limit, :page)
     
     api_request[:limit] ||= 50
+    api_request[:page] ||= 1
+    api_request[:page] = 1 if api_request[:page].to_i == 0
+    api_request[:start] = (api_request[:page].to_i-1) * api_request[:limit].to_i
     
     api_request
   end
