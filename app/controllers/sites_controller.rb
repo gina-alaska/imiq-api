098886b89609803f0529f01f989bb025d1105e37
Will Fisher <will@teknofire.net>
2013-11-19 14:09:34 -0900

    adding limit to default site return

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -1,6 +1,6 @@
 class SitesController < ApplicationController
   def index
-    @sites = Site.all
+    @sites = Site.limit(api_params[:limit])
     
     respond_to do |format|
       format.html
@@ -15,5 +15,21 @@ class SitesController < ApplicationController
         )
       }
     end
+  end
+  # 
+  # def update
+  #   @site = Site.find(params[:id])
+  #   
+  #   @site.update_attributes(params[:site])
+  # end
+  
+  protected
+  
+  def api_params
+    api_request = params.permit(:limit)
+    
+    api_request[:limit] ||= 50
+    
+    api_request
   end
 end
