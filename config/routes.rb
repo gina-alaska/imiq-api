8124b9823b4b600dc69db258db2956e9987efbe1
Will Fisher <will@teknofire.net>
2013-11-19 14:45:14 -0900

    getting rid of actions that we don't need

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 ImiqApi::Application.routes.draw do
-  resources :sites
+  resources :sites, except: [:new, :update, :destroy, :create, :edit]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
