b43da0fccbc548e00da772c85ae45baedc2fed53
jdelamere <jen@gina.alaska.edu>
2013-11-20 14:28:49 -0900

    Overriding the default as_json method.

--- app/models/source.rb
+++ app/models/source.rb
@@ -1,3 +1,16 @@
 class Source < ActiveRecord::Base
   has_many :sites, foreign_key: 'sourceid'  
+  
+  # Override the default as_json method to set our default values
+  def as_json(opts = nil)
+    opts ||= {}
+    #opts[:include] ||= []
+    #opts[:include] << :source
+    
+    #opts[:except] ||= []
+    #opts[:except] << :sourceid
+    
+    super(opts)
+  end
+  
 end
