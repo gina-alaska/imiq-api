2c6672c99858ff42fef35bb64ac15dbc0e4f7fa8
Will Fisher <will@teknofire.net>
2013-11-25 12:32:34 -0900

    adding comment about the proper view we need to use

--- app/models/site.rb
+++ app/models/site.rb
@@ -1,4 +1,11 @@
 class Site < ActiveRecord::Base
+  # This is the configuration needed to pull the from the correct view with valid sites
+  # currently we are not using it because the geolocation field in being set to a binary
+  # datatype that we cannot decode
+  
+  # self.table_name = 'seriescatalog_62'
+  # self.primary_key = 'siteid'
+  
   belongs_to :source, foreign_key: 'sourceid'
   has_one :metadata, through: :source
   
@@ -12,7 +19,7 @@ class Site < ActiveRecord::Base
   }
 
   # Override the default as_json method to set our default values
-  def as_json(opts = nil)
+  def as_json(opts = {})
     opts ||= {}
     opts[:include] ||= []
     opts[:include] << :source
