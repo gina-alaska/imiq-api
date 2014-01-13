749b60e23e0e540efb117cf063d7ecb3d35fa7be
Jen Delamere <jen@gina.alaska.edu>
2014-01-13 14:37:09 -0900

    Add "has_data" scope and csv headers.

--- app/models/daily_winddirectiondatavalue.rb
+++ app/models/daily_winddirectiondatavalue.rb
@@ -1,3 +1,9 @@
 class DailyWinddirectiondatavalue < ActiveRecord::Base
   belongs_to :site, foreign_key: 'siteid'
+  scope :has_data, Proc.new{where "datavalue is not null"}
+  
+  def self.csv_header
+    "Wind Direction (deg)"
+  end
+  
 end
--- app/models/daily_windspeeddatavalue.rb
+++ app/models/daily_windspeeddatavalue.rb
@@ -1,3 +1,9 @@
 class DailyWindspeeddatavalue < ActiveRecord::Base
   belongs_to :site, foreign_key: 'siteid'
+  scope :has_data, Proc.new{where "datavalue is not null"}
+  
+  def self.csv_header
+    "Wind Speed (m/s)"
+  end
+  
 end
