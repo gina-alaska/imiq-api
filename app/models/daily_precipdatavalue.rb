8a8d7674c71a1195305e56d7093f65ebe1e833bc
Jen Delamere <jen@gina.alaska.edu>
2014-02-05 13:05:07 -0900

    Update units to mm.

--- app/models/daily_precipdatavalue.rb
+++ app/models/daily_precipdatavalue.rb
@@ -6,7 +6,7 @@ class DailyPrecipdatavalue < ActiveRecord::Base
   PRETTY_NAME = "Daily Precipitation"
   
   def self.csv_header
-    pretty_name + " (inches)"
+    pretty_name + " (mm)"
   end
   
 end
