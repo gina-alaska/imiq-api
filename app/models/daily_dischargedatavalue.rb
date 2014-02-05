26849bebabff3a2151f77bc0cf42b91d2d8945c1
Jen Delamere <jen@gina.alaska.edu>
2014-02-05 13:06:05 -0900

    Update units to cubic meter/sec

--- app/models/daily_dischargedatavalue.rb
+++ app/models/daily_dischargedatavalue.rb
@@ -6,7 +6,7 @@ class DailyDischargedatavalue < ActiveRecord::Base
   PRETTY_NAME = "Daily Discharge"
   
   def self.csv_header
-    pretty_name + " (cfs)"
+    pretty_name + " (m3/sec)"
   end
   
 end
