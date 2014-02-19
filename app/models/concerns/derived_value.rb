76b3d1bca970bb9e5a770a32afa968d179bd29f4
Will Fisher <will@teknofire.net>
2014-02-19 14:34:52 -0900

    adding method to return the proper data stream for the derived value

--- app/models/concerns/derived_value.rb
+++ app/models/concerns/derived_value.rb
@@ -1,3 +1,4 @@
+# encoding: utf-8
 module DerivedValue
   extend ActiveSupport::Concern
   
@@ -5,8 +6,12 @@ module DerivedValue
     belongs_to :site, foreign_key: 'siteid'
     belongs_to :variable, foreign_key: 'originalvariableid'
     has_one :source, through: :site
-    has_many :datastreams, through: :variable
+    has_many :datastreams, :through => :site
   
-    scope :has_data, Proc.new{where "datavalue is not null"}
+    scope :has_data, ->{ where "datavalue is not null" }
+    
+    def datastream
+      self.datastreams.where(siteid: self.siteid).first
+    end
   end
 end
