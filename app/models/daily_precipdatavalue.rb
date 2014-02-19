08e371ef96215d0be1822e8eb8a25c10e9e50d2b
Jen Delamere <jen@gina.alaska.edu>
2014-02-19 12:05:22 -0900

    Adding additional relations as has been done for DailyAirtempdatavalue; using new concern for derived value models

--- app/models/daily_precipdatavalue.rb
+++ app/models/daily_precipdatavalue.rb
@@ -1,7 +1,5 @@
 class DailyPrecipdatavalue < ActiveRecord::Base
-  belongs_to :site, foreign_key: 'siteid'
-  scope :has_data, Proc.new{where "datavalue is not null"}
-  
+  include DerivedValue  
   include PrettyName
   PRETTY_NAME = "Daily Precipitation"
   
--- app/models/daily_swedatavalue.rb
+++ app/models/daily_swedatavalue.rb
@@ -1,7 +1,5 @@
 class DailySwedatavalue < ActiveRecord::Base
-  belongs_to :site, foreign_key: 'siteid'
-  scope :has_data, Proc.new{where "datavalue is not null"}
-  
+  include DerivedValue  
   include PrettyName
   PRETTY_NAME = "Daily Snow Water Equivalent"
   
