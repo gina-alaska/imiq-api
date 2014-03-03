61f21fbbfe90e99e8b52c7edf6a4ad7e406981c0
Jen Delamere <jen@gina.alaska.edu>
2014-03-03 11:03:28 -0900

    Checking for presence of index; add index if not detected.

--- db/migrate/20140220002828_add_index_to_hourly_derived.rb
+++ db/migrate/20140220002828_add_index_to_hourly_derived.rb
@@ -1,11 +1,15 @@
 class AddIndexToHourlyDerived < ActiveRecord::Migration
   def change
-    add_index 'hourly_airtempdatavalues', 'siteid'
-    add_index 'hourly_rhdatavalues', 'siteid'
-    add_index 'hourly_precipdatavalues', 'siteid'
-    add_index 'hourly_snowdepthdatavalues', 'siteid'
-    add_index 'hourly_swedatavalues', 'siteid'
-    add_index 'hourly_winddirectiondatavalues', 'siteid'
-    add_index 'hourly_windspeeddatavalues', 'siteid'
+    apply_index 'hourly_airtempdatavalues', 'siteid'
+    apply_index 'hourly_rhdatavalues', 'siteid'
+    apply_index 'hourly_precipdatavalues', 'siteid'
+    apply_index 'hourly_snowdepthdatavalues', 'siteid'
+    apply_index 'hourly_swedatavalues', 'siteid'
+    apply_index 'hourly_winddirectiondatavalues', 'siteid'
+    apply_index 'hourly_windspeeddatavalues', 'siteid'
+  end
+
+  def apply_index(table, field)
+    add_index(table, field) unless index_exists?(table, field)
   end
 end
