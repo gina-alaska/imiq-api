c3ab574583c63f0bd7361bc0793256ab0eeb399f
Jen Delamere <jen@gina.alaska.edu>
2014-03-31 16:23:57 -0800

    Fixing one association.

--- app/models/network.rb
+++ app/models/network.rb
@@ -1,7 +1,7 @@
 class Network < ActiveRecord::Base
   self.table_name = 'networkdescriptions'
   
-  has_many :organizations_sources, foreign_key: 'organizationid'
+  has_many :organizations_sources, foreign_key: 'networkid'
   has_many :sources, through: :organizations_sources
   has_many :sites, through: :sources
   
