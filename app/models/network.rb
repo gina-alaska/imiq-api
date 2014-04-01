686f2332b5a8c34fc29aefdc3482aa200f3f7ff4
Jen Delamere <jen@gina.alaska.edu>
2014-03-31 16:17:05 -0800

    Adding associations for Network(s).

--- app/models/network.rb
+++ app/models/network.rb
@@ -1,3 +1,8 @@
 class Network < ActiveRecord::Base
   self.table_name = 'networkdescriptions'
+  
+  has_many :organizations_sources, foreign_key: 'organizationid'
+  has_many :sources, through: :organizations_sources
+  has_many :sites, through: :sources
+  
 end
--- app/models/source.rb
+++ app/models/source.rb
@@ -2,6 +2,7 @@ class Source < ActiveRecord::Base
   has_many :sites, foreign_key: 'sourceid'
   belongs_to :metadata, foreign_key: 'metadataid'
   has_many :organizations_sources, foreign_key: 'sourceid'
+  has_many :networks, through: :organizations_sources
   has_many :organizations, through: :organizations_sources
   
   # Override the default as_json method to set our default values
