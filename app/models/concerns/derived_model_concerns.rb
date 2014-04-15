281f3cdf6087b82e893fd0ebdf5d183664761511
Will Fisher <will@teknofire.net>
2014-04-14 16:37:28 -0800

    adding new concerns handler for new derived models

---
+++ app/models/concerns/derived_model_concerns.rb
@@ -0,0 +1,19 @@
+# encoding: utf-8
+module DerivedModelConcerns
+  extend ActiveSupport::Concern
+
+  included do
+    belongs_to :site, foreign_key: 'siteid'
+    belongs_to :variable, foreign_key: 'variableid'
+    has_one :source, through: :site
+    has_many :datastreams, :through => :site
+
+    scope :has_data, ->{ where "datavalue is not null" }
+    scope :startdate, -> datetime { where "utcdatetime >= ?", datetime }
+    scope :enddate, -> datetime { where "utcdatetime <= ?", datetime }
+
+    def datastream
+      self.datastreams.where(siteid: self.siteid).first
+    end
+  end
+end
