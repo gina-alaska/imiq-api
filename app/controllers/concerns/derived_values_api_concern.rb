a0d3ff43c3f7e0a2b984acd45596b8c7812a3588
Will Fisher <will@teknofire.net>
2014-10-27 15:21:26 -0800

    cleaning up some formatting for docs

--- app/controllers/concerns/derived_values_api_concern.rb
+++ app/controllers/concerns/derived_values_api_concern.rb
@@ -3,15 +3,49 @@ module DerivedValuesApiConcern
 
   TITLE = {
     title: "Export",
-    description: "The export endpoint accesses summary-products in the Imiq Hydroclimate Database and builds a .csv file for download. 
-    The timestep and the summary-product data field are declared in the URL. "
+    description: "
+The export endpoint accesses summary-products in the Imiq Hydroclimate Database and builds a .csv file for download. 
+The timestep and the summary-product data field are declared in the URL. "
+
   }
   
   API = {
-    '/export/"timestep"_"field".csv' => {
-      description: "Export data with a given timestep and datafield.",
+    '/export/{timestep}_{field}.{format}' => {
+      description: "
+
+Export \"field\" value descriptions:
+
+* airtemp       - air temperature at surface height of 2 m
+* discharge     - river/stream discharge
+* precip        - accumulated precipitation
+* rh            - relative humidity at the surface height of 2 m
+* snowdepth     - accumulated snow depth
+* swe           - snow water equivalent
+* watertemp     - temperature of lakes, etc at the surface
+* winddirection - wind direction near surface
+* windspeed     - wind direction near surface
+      ",
       url: "http://imiq-api.gina.alaska.edu/export/daily_airtemp.csv?siteids=3201&startdate=19940601&enddate=19940731",
       response: "A .csv file will be downloaded to your computer.",
+      url_params: [
+        {
+          name: 'timestep',
+          required: true,
+          values: %w{ hourly daily monthly annual }
+        }, 
+        {
+          name: 'field',
+          required: true,
+          values: %w{ airtemp discharge precip rh snowdepth swe watertemp winddirection windspeed }
+          
+        },
+        {
+          name: 'format',
+          required: false,
+          values: %w{ csv json },
+          default: 'csv'
+        }
+      ],
       api_params: [
         {
           name: 'siteid', 
